b9341f79a35420b25012be29c86e0834d843770f
Incredeble <vaibhav.18bcs1050@abes.ac.in>
2021-04-10 11:27:42 +0530

    first commit

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'coin_data.dart';
+import 'dart:io' show Platform;
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,11 +9,59 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String dropDownValue = 'USD';
+
+  DropdownButton getAndroidDropDown() {
+    List<DropdownMenuItem<String>> dropDownItem = [];
+    for (String currency in currenciesList) {
+      dropDownItem.add(
+        DropdownMenuItem<String>(
+          child: SizedBox(
+            width: 100.0,
+            child: Text(
+              currency,
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          value: currency,
+        ),
+      );
+    }
+    return DropdownButton<String>(
+      dropdownColor: Colors.lightBlueAccent,
+      value: dropDownValue,
+      items: dropDownItem,
+      onChanged: (currency) {
+        setState(() {
+          dropDownValue = currency;
+        });
+      },
+    );
+  }
+
+  CupertinoPicker getiOSDropDown() {
+    List<Text> dropDownItem = [];
+    for (String currency in currenciesList) {
+      dropDownItem.add(
+        Text(currency),
+      );
+    }
+    return CupertinoPicker(
+      itemExtent: 32.0,
+      onSelectedItemChanged: (index) {
+        print(index);
+      },
+      children: dropDownItem,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('🤑 Coin Ticker'),
+        centerTitle: true,
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -38,11 +89,15 @@ class _PriceScreenState extends State<PriceScreen> {
             ),
           ),
           Container(
-            height: 150.0,
+            margin: EdgeInsets.symmetric(horizontal: 90.0, vertical: 30.0),
+            height: 50.0,
+            decoration: BoxDecoration(
+              color: Colors.lightBlueAccent,
+              shape: BoxShape.rectangle,
+              borderRadius: BorderRadius.circular(30.0),
+            ),
             alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: null,
+            child: Platform.isIOS ? getiOSDropDown() : getAndroidDropDown(),
           ),
         ],
       ),
